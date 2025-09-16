e0da3f58b919617cca1adef52e162faceb815944
qithaka <qithaka@gmail.com>
2025-09-16 11:45:27 +0300

    style(`/components/layout`): enhance header styles in `Header.tsx`
    Refined the `Header` component by adding Tailwind utility classes for height, flex layout, sticky positioning, spacing, z-index, and a styled pseudo-element for background, blur, and border effects.
    
    This update improves the visual design and usability of the header by ensuring it remains visible during scrolling while providing a polished background and accent styling that aligns with the theme.

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -3,5 +3,12 @@ interface Props {
 }
 
 export default function Header({ className }: Props) {
-  return <header className={className}></header>;
+  return (
+    <header
+      className={[
+        className,
+        'h-14 min-h-14 flex flex-row items-center justify-between sticky top-0 gap-7 z-50 before:absolute before:top-0 before:content-[""] before:block before:h-14 before:bg-[var(--accent-color)] before:backdrop-blur-lg before:w-full before:-z-10 before:border-b before:border-b-[var(--highlight-color)] before:left-0',
+      ].join(' ')}
+    ></header>
+  );
 }
